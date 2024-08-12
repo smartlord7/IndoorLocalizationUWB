7a937fbbc2311c22d2e1251c2110715ced57c705
Sancho Amaral Simões <smartlord7@gmail.com>
2024-08-12 21:52:01 +0100

    MASTER: Fix WLS estimator.

--- AnchorPositionCalibration/estimators/weightedLeastSquares.m
+++ AnchorPositionCalibration/estimators/weightedLeastSquares.m
@@ -1,19 +1,26 @@
-function estimatedAnchors = weightedLeastSquares(distances_noisy, trueAnchors, tagPos)
+function estimatedAnchors = weightedLeastSquares(distances_noisy, initialAnchors, tagPos)
     % Weighted Least Squares for anchor position estimation
-    numAnchors = size(trueAnchors, 1);
-    weights = 1 ./ (distances_noisy + 0.1);
-    W = diag(weights);
+    numAnchors = size(initialAnchors, 1);
+    weights = 1 ./ (distances_noisy .^ 2); % Inverse of squared noisy distances
+    W = diag(weights); % Weight matrix
+    
     A = zeros(numAnchors, 3);
     b = zeros(numAnchors, 1);
-
+    
     for i = 1:numAnchors
-        A(i, :) = 2 * (trueAnchors(i, :) - tagPos);
-        b(i) = distances_noisy(i)^2 - sum(trueAnchors(i, :).^2) + sum(tagPos.^2);
+        A(i, :) = 2 * (initialAnchors(i, :) - tagPos);
+        b(i) = distances_noisy(i) ^ 2 - sum(initialAnchors(i, :) .^ 2) + sum(tagPos .^ 2);
     end
-
-    % Solve for the estimated tag position
+    
+    % Solve the weighted least squares problem
     estimatedTagPos = (A' * W * A) \ (A' * W * b);
-
-    % Adjust the anchor positions based on the estimated tag position
-    estimatedAnchors = trueAnchors + repmat(estimatedTagPos' - tagPos, numAnchors, 1);
+    
+    % Adjust anchors based on estimated tag position
+    estimatedAnchors = initialAnchors + repmat(estimatedTagPos' - tagPos, numAnchors, 1);
+    
+    % Correct translation by aligning centroids
+    estimatedCentroid = mean(estimatedAnchors, 1);
+    trueCentroid = mean(initialAnchors, 1);
+    translationCorrection = trueCentroid - estimatedCentroid;
+    estimatedAnchors = estimatedAnchors + repmat(translationCorrection, numAnchors, 1);
 end
