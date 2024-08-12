d7e0314f2e715f66aadcc834266c1c1cc870db91
Sancho Amaral Simões <smartlord7@gmail.com>
2024-08-13 00:35:13 +0100

    MASTER: WIP neural network.

--- AnchorPositionCalibration/src/estimators/custom_local_network/calculateAnchors.m
+++ AnchorPositionCalibration/src/estimators/custom_local_network/calculateAnchors.m
@@ -1,5 +1,6 @@
 function anchors = calculateAnchors(W1, b1, W2, b2, tagPos, numAnchors)
-    hiddenLayer = relu(W1 * tagPos' + b1); % ReLU activation
-    anchors = W2 * hiddenLayer + b2; % Output layer computation
+    % Forward pass through the network
+    hiddenLayer = relu(W1 * tagPos' + b1); % Hidden layer with ReLU activation
+    anchors = W2 * hiddenLayer + b2; % Output layer
     anchors = reshape(anchors, numAnchors, 3); % Reshape into 2D array of anchor coordinates
 end
--- AnchorPositionCalibration/src/estimators/custom_local_network/neuralNetworkCost.m
+++ AnchorPositionCalibration/src/estimators/custom_local_network/neuralNetworkCost.m
@@ -1,9 +1,15 @@
 function cost = neuralNetworkCost(params, distances_noisy, tagPos, numInputs, numHiddenUnits, numOutputs)
+    % Extract parameters
     W1 = reshape(params(1:numHiddenUnits*numInputs), numHiddenUnits, numInputs);
-    b1 = reshape(params(numHiddenUnits*numInputs+1:numHiddenUnits*numInputs+numHiddenUnits), numHiddenUnits, 1);
-    W2 = reshape(params(numHiddenUnits*numInputs+numHiddenUnits+1:end-numOutputs), numOutputs, numHiddenUnits);
-    b2 = reshape(params(end-numOutputs+1:end), numOutputs, 1);
+    b1 = params(numHiddenUnits*numInputs+1:numHiddenUnits*numInputs+numHiddenUnits);
+    idx = numHiddenUnits*numInputs + numHiddenUnits;
 
+    W2 = reshape(params(idx+1:idx+numOutputs*numHiddenUnits), numOutputs, numHiddenUnits);
+    b2 = params(idx+numOutputs*numHiddenUnits+1:end);
+
+    % Forward pass
     estimatedAnchors = calculateAnchors(W1, b1, W2, b2, tagPos, numOutputs / 3);
+
+    % Calculate cost (sum of squared errors)
     cost = sum((sqrt(sum((reshape(estimatedAnchors, [], 3) - tagPos).^2, 2)) - distances_noisy).^2);
 end
